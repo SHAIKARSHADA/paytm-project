206afce4f444f44649d308fa076199e7da6c28b6
Shaik <shaikarshad1792004@gmail.com>
2024-06-29 20:05:13 +0530

    added routing in express for main router and user router

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,12 @@
 const express = require("express");
+const mainRouter = require("./routes/index")
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 
+app.use(cors());
+app.use(bodyParser());
+
+const app = express();
+
+app.use('/api/v1',mainRouter);
